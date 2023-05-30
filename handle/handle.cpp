504932ffc85bdd3c8e9213ec4552f0e4705ec86b
pain1929 <108464892+pain1929@users.noreply.github.com>
2023-05-30 17:39:26 +0800

    Create handle.cpp

---
+++ handle/handle.cpp
@@ -0,0 +1,41 @@
+#include "handle.h"
+LONG_PTR GetPID(const TCHAR* processName)
+{
+	PROCESSENTRY32 pe = { sizeof(pe) };
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (snapshot == NULL)
+		throw std::exception("创建快照失败");
+	bool success;
+	success = Process32First(snapshot, &pe);
+	if (_tcscmp(processName, pe.szExeFile) == 0)
+	{
+		return pe.th32ProcessID;
+	}
+	while (success)
+	{
+		success = Process32Next(snapshot, &pe);
+		if (_tcscmp(processName, pe.szExeFile) == 0)
+		{
+			return pe.th32ProcessID;
+		}
+	}
+	return NULL;
+}
+
+LONG_PTR GetModuleBase(const TCHAR* moduleName , HANDLE handle)
+{
+	HMODULE hmodules[1024];
+	DWORD size;
+	EnumProcessModules(handle, hmodules, sizeof(hmodules), &size);
+	for (size_t i = 0; i < size; i++)
+	{
+		HMODULE mod = hmodules[i];
+		TCHAR name[MAX_PATH];
+		GetModuleBaseName(handle, mod, name, MAX_PATH);
+		if (_tcscmp(moduleName, name) == 0)
+		{
+			return (LONG_PTR)mod;
+		}
+	}
+	return NULL;
+}
